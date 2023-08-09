eb90cffe8a00f7b506b497a0e65f7833ddaa315a
riyeonlee <gjffod100@gmail.com>
2023-08-10 00:04:01 +0900

    Step4

---
+++ meme/View/MemoCell.swift
@@ -0,0 +1,31 @@
+//
+//  MemoCell.swift
+//  meme
+//
+//  Created by t2023-m0063 on 2023/08/10.
+//
+
+import SwiftUI
+
+struct MemoCell: View {
+    @ObservedObject var memo: Memo
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(memo.content)
+                .font(.body)
+                .lineLimit(1)
+            
+            Text(memo.insertDate, style: .date)
+                .font(.caption)
+                .foregroundColor(.secondary)
+            
+        }
+    }
+}
+
+
+struct MemoCell_Previews: PreviewProvider {
+    static var previews: some View {
+        MemoCell(memo: Memo(content: "Test"))
+    }
+}

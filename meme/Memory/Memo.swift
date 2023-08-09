1c287608c17e1a8cb08ba16b6e477a2e8391f4ef
riyeonlee <gjffod100@gmail.com>
2023-08-09 23:38:24 +0900

    Step2

---
+++ meme/Memory/Memo.swift
@@ -0,0 +1,21 @@
+//
+//  Memo.swift
+//  meme
+//
+//  Created by t2023-m0063 on 2023/08/09.
+//
+
+import Foundation
+import SwiftUI
+
+class Memo: Identifiable, ObservableObject {
+    let id: UUID
+    @Published var content: String
+    let insertDate: Date
+    
+    init(content: String, insertDate: Date = Date.now) {
+        id = UUID()
+        self.content = content
+        self.insertDate = insertDate
+    }
+}

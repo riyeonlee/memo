27a816b2f8731d4942a5f5a2c4cb7b8e9e2320c1
riyeonlee <gjffod100@gmail.com>
2023-08-10 00:50:19 +0900

    Step7

---
+++ meme/View/DetailView.swift
@@ -0,0 +1,44 @@
+//
+//  DetailView.swift
+//  meme
+//
+//  Created by t2023-m0063 on 2023/08/10.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    @ObservedObject var memo: Memo
+    
+    @EnvironmentObject var store: MemoStore
+    
+    
+    var body: some View {
+        VStack {
+            ScrollView {
+                VStack{
+                    HStack {
+                        Text(memo.content)
+                            .padding()
+                        
+                        Spacer(minLength: 0)
+                    }
+                    
+                    Text(memo.insertDate, style: .date)
+                        .padding()
+                        .font(.footnote)
+                        .foregroundColor(.secondary)
+                }
+            }
+        }
+        .navigationTitle("메모 보기")
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailView(memo: Memo(content: "Hello"))
+            .environmentObject(MemoStore())
+    }
+}

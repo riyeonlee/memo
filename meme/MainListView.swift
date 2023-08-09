d00a0f468e71f5b6d901b212867972bbf5b58332
riyeonlee <gjffod100@gmail.com>
2023-08-09 23:19:56 +0900

    Step1

---
+++ meme/MainListView.swift
@@ -0,0 +1,20 @@
+//
+//  MainListView.swift
+//  meme
+//
+//  Created by t2023-m0063 on 2023/08/09.
+//
+
+import SwiftUI
+
+struct MainListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MainListView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainListView()
+    }
+}
--- meme/memeApp.swift
+++ meme/memeApp.swift
@@ -13,7 +13,7 @@ struct memeApp: App {
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MainListView()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }

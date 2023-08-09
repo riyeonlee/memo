cc4e65524e15ae05a0239851c55109fe2c2cdeff
riyeonlee <gjffod100@gmail.com>
2023-08-10 01:09:14 +0900

    Step8

--- meme/View/ComposeView.swift
+++ meme/View/ComposeView.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct ComposeView: View {
     @EnvironmentObject var store: MemoStore
     
+    var memo: Memo? = nil
+    
     @Environment(\.dismiss) var dismiss
     
     @State private var content: String = ""
@@ -18,8 +20,13 @@ struct ComposeView: View {
             VStack {
                 TextEditor(text: $content)
                     .padding()
+                    .onAppear {
+                        if let memo = memo {
+                            content = memo.content
+                        }
+                    }
             }
-            .navigationTitle("새 메모")
+            .navigationTitle(memo != nil ? "메모 편집" : "새 메모")
             .navigationBarTitleDisplayMode(.inline)
             .toolbar {
                 ToolbarItemGroup(placement:
@@ -35,7 +42,11 @@ struct ComposeView: View {
                         .navigationBarTrailing)
                 {
                             Button{
-                                store.insert(memo: content)
+                                if let memo = memo {
+                                    store.update(memo: memo, content: content)
+                                } else {
+                                    store.insert(memo: content)
+                                }
                                 
                                 dismiss()
                             } label: {
--- meme/View/DetailView.swift
+++ meme/View/DetailView.swift
@@ -12,6 +12,8 @@ struct DetailView: View {
     
     @EnvironmentObject var store: MemoStore
     
+    @State private var showComposer = false
+    
     
     var body: some View {
         VStack {
@@ -33,12 +35,25 @@ struct DetailView: View {
         }
         .navigationTitle("메모 보기")
         .navigationBarTitleDisplayMode(.inline)
+        .toolbar {
+            ToolbarItemGroup(placement: .bottomBar) {
+                Button {
+                    showComposer = true
+                } label: {
+                    Image(systemName: "square.and.pencil")
+                }
+            }
+        }
+        .sheet(isPresented: $showComposer) {
+            ComposeView(memo: memo)
+        }
     }
 }
-
 struct DetailView_Previews: PreviewProvider {
     static var previews: some View {
-        DetailView(memo: Memo(content: "Hello"))
-            .environmentObject(MemoStore())
+        NavigationView {
+            DetailView(memo: Memo(content: "Hello"))
+                .environmentObject(MemoStore())
+        }
     }
 }

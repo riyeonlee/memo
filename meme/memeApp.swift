f9e09d716e6eec2d0bd535d1f5b2ecf106b1ab15
riyeonlee <gjffod100@gmail.com>
2023-08-09 23:14:16 +0900

    Initial Commit

---
+++ meme/memeApp.swift
@@ -0,0 +1,20 @@
+//
+//  memeApp.swift
+//  meme
+//
+//  Created by t2023-m0063 on 2023/08/09.
+//
+
+import SwiftUI
+
+@main
+struct memeApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
